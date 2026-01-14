0857d785a6284db8895ea210951a015c8bbf9025
Rafael Paraiso <rafaelparaiso.bsu@gmail.com>
2026-01-15 03:30:30 +0400

    Revert "Update vite.config.js"
    This reverts commit bcad4f425ca845e3e52247385821fa65010730e0.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [],
-});
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    open: true
+  }
+})
